3ba547b678307b059c8fcf36e8e32dc0f63b96bc
Admin <Admin@WT11>
2022-09-26 03:14:45 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3._4_20CS1115
+{
+    class Class1
+    {
+        private int l, w,area;
+        public Class1()
+        {
+
+        }
+        public Class1(int a,int b)
+        {
+            l = a;
+            w = b;
+            area = l * w;
+        }
+        public static bool operator > (Class1 obj1,Class1 obj2)
+        {
+            Class1 temp1 = new Class1();
+            Class1 temp2 = new Class1();
+            temp1.area = obj1.area;
+            temp2.area = obj2.area;
+            if (temp1.area > temp2.area)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <(Class1 obj1, Class1 obj2)
+        {
+            Class1 temp1 = new Class1();
+            Class1 temp2 = new Class1();
+            temp1.area = obj1.area;
+            temp2.area = obj2.area;
+            if (temp1.area < temp2.area)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3._4_20CS1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1(2, 4);
+            Class1 obj2 = new Class1(1, 4);
+            if(obj1 > obj2)
+            {
+                Console.WriteLine("park 1 is greater");
+            }
+            else if(obj1 < obj2)
+            {
+                Console.WriteLine("park 2 is greater");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
